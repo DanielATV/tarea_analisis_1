28cf1a1cfbc839a4eca6bb81207d86a77c1dbe6d
AlbireoImma <francisco@ubuntu.ubuntu-domain>
2018-10-13 19:17:01 -0300

    archivo para crear la tabla de usuarios

---
+++ tabla_usuarios.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION pgcrypto;
+
+
+CREATE TABLE users (
+  id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+  email text NOT NULL,
+  password text NOT NULL,
+  name text NOT NULL
+);
