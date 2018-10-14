6ec388411d9f3a203b11bfff6604bcf4eb8ca9df
AlbireoImma <francisco@ubuntu.ubuntu-domain>
2018-10-13 21:07:46 -0300

    Documentado sql

--- tabla_usuarios.sql
+++ tabla_usuarios.sql
@@ -2,8 +2,8 @@ CREATE EXTENSION pgcrypto;
 
 
 CREATE TABLE users (
-  id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-  email text NOT NULL,
-  password text NOT NULL,
-  name text NOT NULL
+  id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY, /* id (aleatoria) del usuario */
+  email text NOT NULL, /* email del usuario */
+  password text NOT NULL, /* contraseña del usuario */
+  name text NOT NULL /* nombre del usuario */
 );
